c8883b91ae904f2ae9296f5438df7ba8c2dc8ef2
lifer-creator <3622103798@qq.com>
2026-09-18 17:04:27 +0800

    第一次提交

---
+++ 猜数字.py
@@ -0,0 +1,18 @@
+import random
+s=random.randint(1,100)
+
+c=6
+while c!=0:
+	print("您还有",c, "次机会")
+	guess=int(input("请输入数字"))
+	if(s>guess):
+		print("偏小了")
+	elif(s<guess):
+		print("偏大了")
+	else:
+		print("猜对了")
+		break
+	c=c-1
+if c==0:
+	print("失败了")
+        
